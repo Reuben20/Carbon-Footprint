ae06e2b7a79fbfe5a88052188f3ed3e39d66fcf0
ruben cam calderon <rubencamcalderon@gmail.com>
2020-06-29 19:25:05 -0500

    orange target eliminated

--- public/app.js
+++ public/app.js
@@ -1,8 +1,18 @@
 var validator = true
 var active = false
 
+document.getElementById('back').addEventListener('click',() => {
+  if(validator === false && active === true){
+    document.getElementById('back').style.visibility = "hidden"
+    document.getElementById('back').style.position = "absolute"
+    document.getElementById('back').style.opacity = "0"
+    move_left()
+    active = false
+  }
+})
+
 function move_animation(){
-  let event_array = [document.getElementById('m-1'),document.getElementById('controller')].forEach(el => el.addEventListener('click',() =>{
+  document.getElementById('controller').addEventListener('click',() =>{
     if(validator === true && active === false){
       active = true
       move_right()
@@ -12,14 +22,13 @@ function move_animation(){
       move_left()
       active = false
     }
-  }))
+  })
 }
 
 move_animation()
 
 function move_left(){
   let div = document.getElementById("scroll")
-  let line_1 = document.getElementById("m-1")
   let element_array = [document.getElementById('p1'),document.getElementById('p2'),document.getElementById("video1")].forEach(i => {
     i.style.visibility = "hidden"
     i.style.position = "absolute"
@@ -30,10 +39,8 @@ function move_left(){
   let i_1 = setInterval(()=>{
     if(x > 0){
       x = x - 0.5
-      line_1.style.left = x + "%"
       div.style.width = x + "%"
     }else{
-      line_1.style.left = "-1%"
       window.clearInterval(i_1)
     }
   },0.1)
@@ -41,16 +48,13 @@ function move_left(){
 
 function move_right(){
   let div = document.getElementById("scroll")
-  let line_1 = document.getElementById("m-1")
   let x = 0
   let i_1 = setInterval(()=>{
     if(x <= 99.5){
       x = x + 0.5
-      line_1.style.left = x + "%"
       div.style.width = x + "%"
     }
     if(x == 100){
-      line_1.style.left = "95%"
       text_draw()
       validator = false
       window.clearInterval(i_1)
@@ -59,7 +63,7 @@ function move_right(){
 }
 
 function text_draw(){
-  let element_array = [document.getElementById('p1'),document.getElementById('p2'),document.getElementById("video1")]
+  let element_array = [document.getElementById('back'),document.getElementById('p1'),document.getElementById('p2'),document.getElementById("video1")]
   element_array.forEach(i => {
     i.style.position = "relative"
     i.style.visibility = "visible"
